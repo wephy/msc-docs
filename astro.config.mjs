5a09ca712ff535777b59e2bd739731ed24c4acda
unknown <joe@wephy.com>
2025-05-22 15:01:00 +0100

    astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://msc.wephy.com', // Correct: This is the public URL
+    // No 'base' property needed
+})
