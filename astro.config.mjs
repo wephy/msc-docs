586368fb3743eaaa64654ac69aebef81bad8fd81
unknown <joe@wephy.com>
2025-06-12 12:20:26 +0100

    1206

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +13,13 @@ export default defineConfig({
 			title: 'MSc Dissertation',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
+				{
+					label: 'Notes',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: '2025-06-12', slug: 'notes/2025_06_12'},
+					],
+				},
 				{
 					label: 'Theory',
 					items: [
@@ -29,6 +39,19 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
+			head: [
+				{
+				  tag: 'link',
+				  attrs: {
+					rel: 'stylesheet',
+					href: 'https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css',
+				  },
+				},
+			  ],
 		}),
 	],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	  },
 });
--- src/content/docs/theory/sketchysgd.md
+++ src/content/docs/theory/sketchysgd.md
@@ -2,3 +2,14 @@
 title: "SketchySGD"
 description: Pratik Rathore 1 Weimu Lei 2 Zachary Frangella 3 Lu Lu 4 Madeleine Udell 2 3
 ---
+
+
+
+
+<!-- #### Algorithm: SketchySGD (Practical Version)
+
+Input: 
+
+for $k = 0, 1,\dots,m-1$ do
+
+$\quad$ Sample a batch $B_k$ -->
